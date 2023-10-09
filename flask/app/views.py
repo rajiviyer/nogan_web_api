62719c24e9597bfdf5386ececc6c912bfc40a885
Rajiv Iyer <raju.rgi@gmail.com>
2023-10-09 20:32:05 +0530

    Added debug statements, volumes info in docker compose

--- flask/app/views.py
+++ flask/app/views.py
@@ -17,15 +17,17 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
+    DATA_DIR = "./app/data"
     try:
         # Get user inputs from the form
         file = request.files['file']
         file_name = file.filename
         file_type = request.form['fileType']
         delimiter = request.form['delimiter']
+        print(f"Current path is: {os. getcwd()}")
 
         # Save the uploaded file
-        file.save(os.path.join('data', file_name))
+        file.save(os.path.join(DATA_DIR, file_name))
 
         # Redirect to the second page for column selection and row generation
         return redirect(url_for('generate', file_name = file_name, file_type=file_type, delimiter=delimiter))
@@ -35,21 +37,28 @@ def process():
             
 @app.route('/generate', methods=['GET', 'POST'])
 def generate():
+    DATA_DIR = "./app/data"
     if request.method == 'GET':
+        DEBUG_LOCATION="Reading Data"
         try:
             file_name = request.args.get('file_name')
             file_type = request.args.get('file_type')
-            delimiter = request.args.get('delimiter')             
+            delimiter = request.args.get('delimiter')
+                         
             # Load data into a pandas DataFrame based on file type
             print(f"File Type: {file_type}")
             if file_type == 'csv':
-                df = pd.read_csv(os.path.join('data', file_name), delimiter=delimiter)
+                DEBUG_STEP="Reading CSV File"
+                df = pd.read_csv(os.path.join(DATA_DIR, file_name), delimiter=delimiter)
             elif file_type == 'text':
-                df = pd.read_csv(os.path.join('data', file_name), delimiter=delimiter)
+                DEBUG_STEP="Reading Text File"
+                df = pd.read_csv(os.path.join(DATA_DIR, file_name), delimiter=delimiter)
             elif file_type == 'excel':
-                df = pd.read_excel(os.path.join('data', file_name))            
+                DEBUG_STEP="Reading Excel File"
+                df = pd.read_excel(os.path.join(DATA_DIR, file_name))            
 
             # Get column names for category selection
+            DEBUG_STEP="Getting Columns List from Data"
             columns = df.columns.tolist()
             print(f"Columns before going to Generate Page: {columns}")
             
@@ -65,13 +74,13 @@ def generate():
                                    )
         
         except Exception as e:
-            error_message = f'Error: {str(e)}'
-            print(error_message)
-            return render_template('generate.html', 
+            error_message = f'Error in {DEBUG_LOCATION}({DEBUG_STEP}): {str(e)}'
+            return render_template('results.html', 
                                    success_message=None,
                                    error_message=error_message)                
 
     elif request.method == 'POST':
+        DEBUG_LOCATION = "Processing Data"
         try:
             action_selected = request.form['action']
             category_columns = request.form.getlist('category_columns')
@@ -85,9 +94,11 @@ def generate():
             seed = np.random.randint(low=1, high=9999999, size=1)
             
             if file_type == "excel":
-                orig_data = pd.read_excel(os.path.join('data', file_name))
+                DEBUG_STEP="Reading Excel Data"
+                orig_data = pd.read_excel(os.path.join(DATA_DIR, file_name))
             else:
-                orig_data = pd.read_csv(os.path.join('data', file_name),
+                DEBUG_STEP="Reading Text/CSV Data"
+                orig_data = pd.read_csv(os.path.join(DATA_DIR, file_name),
                                         delimiter=delimiter)
                                 
             orig_data = orig_data.dropna()
@@ -95,6 +106,7 @@ def generate():
             print(f"Datafile Shape: {orig_data.shape}")
 
             if action_selected == 'validate':
+                DEBUG_LOCATION="Data Validation"
                 num_nodes = int(request.form['valNumNodes'])
                 train_data = orig_data.sample(frac=0.5)
                 val_data = orig_data.drop(train_data.index)
@@ -107,6 +119,7 @@ def generate():
                             wrap_category_columns(val_data,
                                                   category_columns)                               
                 # Train NoGAN
+                DEBUG_STEP="getting Bins"
                 bins_json = request.form["binsText"]               
                 if bins_json:
                     bins_json = json.loads(bins_json)
@@ -114,7 +127,8 @@ def generate():
                 else:
                     bins = [100] * len(train_data.columns)
                 print(f"bins json: {bins_json}") 
-                                
+                
+                DEBUG_STEP="getting Stretch Type"
                 stretch_type_json = request.form["StretchTypeText"]
                 if stretch_type_json:
                     stretch_type_json = json.loads(stretch_type_json)
@@ -124,6 +138,7 @@ def generate():
                     stretch_type = ["Uniform"] * len(train_data.columns)
                 print(f"stretch type json: {stretch_type_json}")
                 
+                DEBUG_STEP="getting Stretch Values"
                 stretch_val_json = request.form["stretchValText"]
                 if stretch_val_json:
                     stretch_val_json = json.loads(stretch_val_json)
@@ -132,34 +147,43 @@ def generate():
                 else:
                     stretch = [1.0] * len(train_data.columns)
                 print(f"stretch val json: {stretch_val_json}")
-                                
+                
+                DEBUG_STEP = "instantiating NoGAN Model"
                 nogan = NoGANSynth(train_data,
                                 random_seed=seed)
                 
+                DEBUG_STEP = "fitting NoGAN Model"
                 nogan.fit(bins = bins)
                 
                 num_rows = len(train_data)
+                
+                DEBUG_STEP="generating Synthetic Data"
                 synth_data = \
                     nogan.generate_synthetic_data(no_of_rows = num_rows,
                                                   stretch_type = stretch_type,
                                                   stretch = stretch)
                 
-                # Calculate ECDFs & KS Stats 
+                # Calculate ECDFs & KS Stats
+                DEBUG_STEP="calculating ECDF for Validation and Synthetic Data"
                 _, ecdf_val1, ecdf_nogan_synth = \
                             multivariate_ecdf(val_data, 
                                               synth_data, 
                                               n_nodes = num_nodes,
                                               verbose = False,
                                               random_seed=seed)
-                            
+                
+                DEBUG_STEP="calculating ECDF for Validation and Training Data"
                 _, ecdf_val2, ecdf_train = \
                             multivariate_ecdf(val_data, 
                                               train_data, 
                                               n_nodes = num_nodes,
                                               verbose = False,
                                               random_seed=seed)
-                            
+                
+                DEBUG_STEP="calculating KS Statistic for Validation and Synthetic Data"
                 ks_stat = ks_statistic(ecdf_val1, ecdf_nogan_synth)
+                
+                DEBUG_STEP="calculating KS Statistic for Validation and Training Data"
                 base_ks_stat = ks_statistic(ecdf_val2, ecdf_train)
                 success_message = f"KS Statistic is: {ks_stat:0.4f}\nBase KS Statistic is: {base_ks_stat:0.4f}"
 
@@ -168,7 +192,7 @@ def generate():
                                 'file_location': None, 
                                 'error_message': None})
             else:
-                print("In Generate")
+                DEBUG_LOCATION = "Data Generation"
                 num_rows = int(request.form['genNumRows'])
                 print(f"Num Rows: {num_rows}")
                 try:             
@@ -177,6 +201,7 @@ def generate():
                     ks_stat_selected = False
                 print(f"KS Selected Value:{ks_stat_selected}")
                 if category_columns:
+                    DEBUG_STEP = "wrapping category columns"
                     wrapped_data, idx_to_key, _ = \
                             wrap_category_columns(orig_data,
                                                 category_columns)
@@ -184,12 +209,15 @@ def generate():
                 
                 # Train NoGAN
                 bins_json = request.form["binsText"]
+                
+                DEBUG_STEP = "getting Bins"
                 if bins_json:
                     bins_json = json.loads(bins_json)
                     bins = [bins_json[key] for key in bins_json]
                 else:
                     bins = [100] * len(train_data.columns)
                 
+                DEBUG_STEP = "getting Stretch Type"
                 stretch_type_json = request.form["StretchTypeText"]
                 if stretch_type_json:
                     stretch_type_json = json.loads(stretch_type_json)
@@ -198,6 +226,7 @@ def generate():
                 else:
                     stretch_type = ["Uniform"] * len(train_data.columns)
                 
+                DEBUG_STEP = "getting Stretch Values"
                 stretch_val_json = request.form["stretchValText"]
                 if stretch_val_json:
                     stretch_val_json = json.loads(stretch_val_json)
@@ -205,12 +234,15 @@ def generate():
                         [stretch_val_json[key] for key in stretch_val_json]
                 else:
                     stretch = [1.0] * len(train_data.columns)
-                                
+                
+                DEBUG_STEP = "instantiating NoGAN Model"
                 nogan = NoGANSynth(orig_data,
                                 random_seed=seed)
                 
+                DEBUG_STEP = "fitting NoGAN Model"                
                 nogan.fit(bins = bins)
                 
+                DEBUG_STEP = "generating Synthetic Data"            
                 synth_data = \
                     nogan.generate_synthetic_data(no_of_rows = num_rows,
                                                   stretch_type = stretch_type,
@@ -218,6 +250,8 @@ def generate():
                 
                 if ks_stat_selected:
                     num_nodes = int(request.form['genNumNodes'])
+                    
+                    DEBUG_STEP="calculating ECDF for Original and Synthetic Data"
                     # Calculate ECDFs & KS Stats 
                     _, ecdf_train, ecdf_nogan_synth = \
                                 multivariate_ecdf(orig_data, 
@@ -225,10 +259,12 @@ def generate():
                                                   n_nodes = num_nodes,
                                                   verbose = False,
                                                   random_seed=seed)
-                                
+                    
+                    DEBUG_STEP="calculating KS Statistic for Original and Synthetic Data"
                     ks_stat = ks_statistic(ecdf_train, ecdf_nogan_synth)            
 
                 if category_columns:
+                    DEBUG_STEP="unwrapping Category Columns"
                     generated_data = \
                     unwrap_category_columns(data=synth_data,
                                             idx_to_key=idx_to_key,
@@ -241,7 +277,7 @@ def generate():
                 timestamp = int(time.time())
                 print(f"Filname type: {type(file_name)}")
                 csv_filename = f"result_{file_name.split('.')[0]}_{timestamp}.csv"
-                generated_data.to_csv(os.path.join('data', csv_filename), index=False)
+                generated_data.to_csv(os.path.join(DATA_DIR, csv_filename), index=False)
                 file_location = f"/download/{csv_filename}"
 
                 if ks_stat_selected:
@@ -254,12 +290,17 @@ def generate():
                                 'error_message': None})
 
         except Exception as e:
-            error_message = f'Error: {str(e)}'
+            if DEBUG_STEP.lower() in ["getting bins", "getting stretch type", "getting stretch values"]:
+                error_message = f'Error in {DEBUG_LOCATION}({DEBUG_STEP}): Please check the json format'
+            else:                
+                error_message = f'Error in {DEBUG_LOCATION}({DEBUG_STEP}): {str(e)}'
+            
             return jsonify({'success_message': None,
                             'file_location': None,
                             'error_message': error_message})
 
 @app.route('/download/<filename>')
 def download(filename):
+    DATA_DIR="./app/data"
     # Serve the file for download from the 'data' directory
-    return send_from_directory('data', filename, as_attachment=True)
+    return send_from_directory(DATA_DIR, filename, as_attachment=True)
